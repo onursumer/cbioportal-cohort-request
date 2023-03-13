16e2d4851d910318dbd26e4be650338a3d6de80f
Onur Sumer <s.onur.sumer@gmail.com>
2023-03-13 14:45:45 -0400

    add basic form validation

--- apps/cohort-request-frontend/src/app/cohort-request-form/CohortRequestForm.tsx
+++ apps/cohort-request-frontend/src/app/cohort-request-form/CohortRequestForm.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Form, OverlayTrigger, Popover } from 'react-bootstrap';
 
 import styles from './CohortRequestForm.module.scss';
@@ -61,14 +61,24 @@ function SubsetInputForm(props: { subsetType: SubsetType }) {
           Please enter the cBioPortal study ID you want to subset{' '}
           <StudyIdInfo />
         </Form.Label>
-        <Form.Control type="text" placeholder="Enter study ID" />
+        <Form.Control
+          required={true}
+          type="text"
+          placeholder="Enter study ID"
+        />
+        <Form.Control.Feedback type="invalid">
+          Please enter study ID(s).
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className="mb-3" controlId="subsetFormCaseId">
         <Form.Label>
           Please enter the sample/patient IDs from the cBioPortal study you want
           to subset
         </Form.Label>
-        <Form.Control as="textarea" rows={10} />
+        <Form.Control required={true} as="textarea" rows={10} />
+        <Form.Control.Feedback type="invalid">
+          Please enter the sample/patient IDs.
+        </Form.Control.Feedback>
       </Form.Group>
     </>
   );
@@ -78,22 +88,47 @@ export function CohortRequestForm(props: CohortRequestFormProps) {
   const [subsetType, setSubsetType] = useState<SubsetType | undefined>(
     undefined
   );
+  const [validated, setValidated] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // do not trigger an actual form submit event
+    e.preventDefault();
+    e.stopPropagation();
+    setValidated(true);
+
+    const form = e.currentTarget;
+    const isValid = form.checkValidity();
+    setValid(isValid);
+
+    if (isValid) {
+      // all valid, ready to call the corresponding API
+      // TODO fire API call
+    }
+  };
 
   return (
-    <Form>
+    <Form noValidate={true} validated={validated} onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="mainFormName">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
+        <Form.Control required={true} type="text" placeholder="Enter name" />
+        <Form.Control.Feedback type="invalid">
+          Please enter your name.
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className="mb-3" controlId="mainFormId">
         <Form.Label>MSK ID</Form.Label>
-        <Form.Control type="text" placeholder="Enter MSK ID" />
+        <Form.Control required={true} type="text" placeholder="Enter MSK ID" />
+        <Form.Control.Feedback type="invalid">
+          Please enter your MSK ID.
+        </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className="mb-3" controlId="mainFormSubsetSelector">
         <Form.Label>
           Select the type of study <FontAwesomeIcon icon={faArrowDown} />
         </Form.Label>
         <Form.Check
+          required={true}
           label="Subset an existing cBioPortal study"
           name="subset"
           type="radio"
@@ -101,6 +136,7 @@ export function CohortRequestForm(props: CohortRequestFormProps) {
           onChange={() => setSubsetType(SubsetType.SingleStudy)}
         />
         <Form.Check
+          required={true}
           label="Subset and/or merge existing cBioPortal studies"
           name="subset"
           type="radio"
@@ -115,7 +151,7 @@ export function CohortRequestForm(props: CohortRequestFormProps) {
         </Form.Label>
         <Form.Control type="text" placeholder="Enter users" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button disabled={valid} variant="primary" type="submit">
         Submit
       </Button>
     </Form>
