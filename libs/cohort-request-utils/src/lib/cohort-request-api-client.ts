b09351c0ac711aeb44e64eeeb0f9f989e7110736
Onur Sumer <s.onur.sumer@gmail.com>
2023-12-01 14:53:40 -0500

    fix api client methods

--- libs/cohort-request-utils/src/lib/cohort-request-api-client.ts
+++ libs/cohort-request-utils/src/lib/cohort-request-api-client.ts
@@ -2,19 +2,20 @@ import {
   CohortRequest,
   CohortRequestResponse,
   Event,
+  Job,
 } from './cohort-request-model';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export async function sendCohortRequest(
   request: CohortRequest
-): Promise<CohortRequestResponse> {
+): Promise<AxiosResponse<CohortRequestResponse>> {
   return axios.post('/api/cohort-request', request);
 }
 
-export async function fetchAllEvents(): Promise<Event[]> {
+export async function fetchAllEvents(): Promise<AxiosResponse<Event[]>> {
   return axios.get('/api/event');
 }
 
-export async function fetchAllJobs(): Promise<CohortRequestResponse> {
-  return axios.post('/api/job');
+export async function fetchAllJobs(): Promise<AxiosResponse<Job[]>> {
+  return axios.get('/api/job');
 }
