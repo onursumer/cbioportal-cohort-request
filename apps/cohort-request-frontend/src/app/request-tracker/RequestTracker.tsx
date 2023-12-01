b1411642563d04e13ef2c9854da1d3c4280b3d11
Onur Sumer <s.onur.sumer@gmail.com>
2023-12-01 18:20:59 -0500

    add more row data

--- apps/cohort-request-frontend/src/app/request-tracker/RequestTracker.tsx
+++ apps/cohort-request-frontend/src/app/request-tracker/RequestTracker.tsx
@@ -8,6 +8,7 @@ import {
   fetchAllEvents,
   fetchAllJobs,
   getRequestStatusFromEvents,
+  groupEventsByJobIdIgnoreDuplicates,
   Job,
 } from '@cbioportal-cohort-request/cohort-request-utils';
 import styles from './RequestTracker.module.scss';
@@ -17,8 +18,11 @@ export interface RequestTrackerProps {}
 
 function generateRowData(jobs: Job[], events: Event[]) {
   const statusMap = getRequestStatusFromEvents(events);
+  const eventsByJobId = groupEventsByJobIdIgnoreDuplicates(events);
+
   return jobs.map((job) => ({
     ...job,
+    events: eventsByJobId[job.jobId],
     status: statusMap[job.jobId],
   }));
 }
@@ -33,11 +37,18 @@ export function RequestTracker(props: RequestTrackerProps) {
   }, []);
 
   const [rowData, setRowData] = useState<Job[]>([]);
+
+  // TODO format columns, add filters & search
   const [colDefs, setColDefs] = useState([
     { field: 'jobId' },
     { field: 'requestDate' },
     { field: 'requesterId' },
     { field: 'requesterName' },
+    { field: 'studyIds' },
+    { field: 'caseIds' },
+    { field: 'users' },
+    { field: 'additionalData' },
+    { field: 'events' },
     { field: 'status' },
   ]);
 
--- libs/cohort-request-utils/src/lib/cohort-request-status.ts
+++ libs/cohort-request-utils/src/lib/cohort-request-status.ts
@@ -4,24 +4,29 @@ import { CohortRequestStatus, Event } from './cohort-request-model';
 export function getRequestStatusFromEvents(
   events: Event[]
 ): Dictionary<CohortRequestStatus> {
+  return chain(groupEventsByJobIdIgnoreDuplicates(events))
+    .map((value) => maxBy(value, (v) => v.eventDate))
+    .map((event) => ({
+      ...event,
+      // if no 'Complete' event logged for a certain job, then assume that it has failed
+      status:
+        event.status === CohortRequestStatus.Complete
+          ? CohortRequestStatus.Complete
+          : CohortRequestStatus.Error,
+    }))
+    .keyBy((d) => d.jobId)
+    .transform((result, value, key) => {
+      result[key] = value.status;
+    }, {})
+    .value();
+}
+
+export function groupEventsByJobIdIgnoreDuplicates(events: Event[]) {
   return (
     chain(events)
       // ignore duplicate events
       .filter((event) => event.status !== CohortRequestStatus.Duplicate)
       .groupBy((d) => d.jobId)
-      .map((value) => maxBy(value, (v) => v.eventDate))
-      .map((event) => ({
-        ...event,
-        // if no 'Complete' event logged for a certain job, then assume that it has failed
-        status:
-          event.status === CohortRequestStatus.Complete
-            ? CohortRequestStatus.Complete
-            : CohortRequestStatus.Error,
-      }))
-      .keyBy((d) => d.jobId)
-      .transform((result, value, key) => {
-        result[key] = value.status;
-      }, {})
       .value()
   );
 }
