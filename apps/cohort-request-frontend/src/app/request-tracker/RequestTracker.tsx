c403d96297994c57edbbb0b5bbbc7a8fe754faed
Onur Sumer <s.onur.sumer@gmail.com>
2023-12-04 19:03:21 -0500

    add details modal

--- apps/cohort-request-frontend/src/app/request-tracker/RequestTracker.tsx
+++ apps/cohort-request-frontend/src/app/request-tracker/RequestTracker.tsx
@@ -1,5 +1,7 @@
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import { filesize } from 'filesize';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css'; // Core CSS
 import 'ag-grid-community/styles/ag-theme-quartz.css'; // Theme
@@ -12,11 +14,17 @@ import {
   groupEventsByJobId,
   Job,
 } from '@cbioportal-cohort-request/cohort-request-utils';
+import IconWithTooltip from '../icon-with-tooltip/IconWithTooltip';
 
 /* eslint-disable-next-line */
 export interface RequestTrackerProps {}
 
-function generateRowData(jobs: Job[], events: Event[]) {
+interface RowDatum extends Job {
+  events: Event[];
+  status: CohortRequestStatus;
+}
+
+function generateRowData(jobs: Job[], events: Event[]): RowDatum[] {
   const statusMap = getRequestStatusFromEvents(events);
   const eventsByJobId = groupEventsByJobId(events);
 
@@ -39,39 +47,78 @@ function StatusRenderer(props: { value: CohortRequestStatus }) {
   return <strong className={statusStyleMap[props.value]}>{props.value}</strong>;
 }
 
-function EventRenderer(props: { value: Event[] }) {
-  const errorCount = props.value.filter(
-    (e) => e.status === CohortRequestStatus.Error
-  ).length;
-  const duplicateRequestCount = props.value.filter(
-    (e) => e.status === CohortRequestStatus.Duplicate
-  ).length;
-
-  const aditionalInfo =
-    errorCount > 1 || duplicateRequestCount > 0 ? (
-      <>
-        ({errorCount > 1 && <>Retries: {errorCount - 1}</>}
-        {errorCount > 1 && duplicateRequestCount > 0 && <>,</>}
-        {duplicateRequestCount > 0 && <>Duplicates: {duplicateRequestCount}</>})
-      </>
-    ) : undefined;
-
+function EventColumn(props: { value: Event[]; onActivateDetails: () => void }) {
   return (
     <span>
-      {props.value.length} {aditionalInfo}
+      {props.value.length}
+      <Button variant="link" onClick={props.onActivateDetails}>
+        <IconWithTooltip
+          icon={faQuestionCircle}
+          tooltipContent={<>click to see details</>}
+        />
+      </Button>
     </span>
   );
 }
 
-function AdditionalDataRenderer(props: {
-  value: {
+function DetailsModal(props: { rowDatum?: RowDatum; onHide?: () => void }) {
+  const events = props.rowDatum?.events.map((event) => ({
+    ...event,
+    requesterId: event.requesterId || props.rowDatum?.requesterId,
+    requesterName: event.requesterName || props.rowDatum?.requesterName,
+    users: event.users || props.rowDatum?.users,
+    additionalData: event.additionalData || props.rowDatum?.additionalData,
+  }));
+
+  return (
+    <Modal
+      show={props.rowDatum !== undefined}
+      centered={true}
+      onHide={props.onHide}
+      size="xl"
+    >
+      <Modal.Header closeButton={true}>
+        <Modal.Title>Job {props.rowDatum?.jobId}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <EventTable events={events} />
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+function EventTable(props: { events?: Event[] }) {
+  const colDefs = [
+    { field: 'eventDate', valueFormatter: dateFormatter },
+    { field: 'requesterId', filter: true },
+    { field: 'requesterName', filter: true },
+    { field: 'users', valueFormatter: stringArrayFormatter, filter: true },
+    { field: 'additionalData', cellRenderer: AdditionalDataColumn },
+    { field: 'status', cellRenderer: StatusRenderer, filter: true },
+  ];
+
+  return (
+    <div className="ag-theme-quartz" style={{ height: 600 }}>
+      <AgGridReact
+        rowData={props.events}
+        columnDefs={colDefs}
+        enableCellTextSelection={true}
+      />
+    </div>
+  );
+}
+
+function AdditionalDataColumn(props: {
+  value?: {
     filename: string;
     size: number;
   }[];
 }) {
   return (
     <span>
-      {props.value.map((d) => `${d.filename} (${filesize(d.size)})`).join(', ')}
+      {props.value
+        ?.map((d) => `${d.filename} (${filesize(d.size)})`)
+        .join(', ')}
     </span>
   );
 }
@@ -80,8 +127,8 @@ function dateFormatter(props: { value: string }) {
   return new Date(props.value).toLocaleString();
 }
 
-function stringArrayFormatter(props: { value: string[] }) {
-  return props.value.join(', ');
+function stringArrayFormatter(props: { value?: string[] }) {
+  return props.value?.join(', ');
 }
 
 export function RequestTracker(props: RequestTrackerProps) {
@@ -93,7 +140,19 @@ export function RequestTracker(props: RequestTrackerProps) {
     });
   }, []);
 
-  const [rowData, setRowData] = useState<Job[]>([]);
+  const [rowData, setRowData] = useState<RowDatum[]>([]);
+  const [selectedRowDatum, setSelectedRowDatum] = useState<
+    RowDatum | undefined
+  >(undefined);
+
+  const eventRenderer = (props: { value: Event[]; data: RowDatum }) => {
+    return (
+      <EventColumn
+        {...props}
+        onActivateDetails={() => setSelectedRowDatum(props.data)}
+      />
+    );
+  };
 
   const colDefs = [
     { field: 'jobId' },
@@ -103,14 +162,17 @@ export function RequestTracker(props: RequestTrackerProps) {
     { field: 'studyIds', valueFormatter: stringArrayFormatter, filter: true },
     { field: 'caseIds', valueFormatter: stringArrayFormatter, filter: true },
     { field: 'users', valueFormatter: stringArrayFormatter, filter: true },
-    { field: 'additionalData', cellRenderer: AdditionalDataRenderer },
-    { field: 'events', cellRenderer: EventRenderer },
+    { field: 'additionalData', cellRenderer: AdditionalDataColumn },
+    { field: 'events', cellRenderer: eventRenderer },
     { field: 'status', cellRenderer: StatusRenderer, filter: true },
-    // TODO add an expander/modal for a detailed view
   ];
 
   return (
     <div className="ag-theme-quartz" style={{ height: 600 }}>
+      <DetailsModal
+        rowDatum={selectedRowDatum}
+        onHide={() => setSelectedRowDatum(undefined)}
+      />
       <AgGridReact
         rowData={rowData}
         columnDefs={colDefs}
