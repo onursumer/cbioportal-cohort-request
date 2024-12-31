c2a5220315280dc5c270caea29562f00315ccad9
Onur Sumer <s.onur.sumer@gmail.com>
2024-12-31 18:26:41 -0500

    fix broken test

--- apps/cohort-request-frontend/src/app/request-tracker/RequestTracker.spec.tsx
+++ apps/cohort-request-frontend/src/app/request-tracker/RequestTracker.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
 
 import RequestTracker from './RequestTracker';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('RequestTracker', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<RequestTracker />);
+    const { baseElement } = render(
+      <BrowserRouter>
+        <RequestTracker />
+      </BrowserRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
